29c48d470456d032c65259593f8db529928b06fc
CaycePollard32 <CaycePollard32@users.noreply.github.com>
2021-02-23 15:36:55 +0100

    Opdracht 1ste deel is klaar

--- 210223_Syntra_NETCORE_opdracht/Controllers/GetalController.cs
+++ 210223_Syntra_NETCORE_opdracht/Controllers/GetalController.cs
@@ -1,16 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _210223_Syntra_NETCORE_opdracht.Controllers
 {
-    public class GetalController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetalController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+
+        public ActionResult<int> LeesHetGetal()
         {
-            return View();
+            string text = System.IO.File.ReadLines(@"C:\Users\emese\source\repos\210223_Syntra_NETCORE_opdracht\210223_Syntra_NETCORE_opdracht\TextFile\MijnGetal.txt").First();
+
+            if (text == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(Convert.ToInt32(text));
+
+
         }
+
+
+
+        [HttpPost("Post1")]
+
+        public ActionResult<int> SchrijfHetGetal(string aText)
+        {
+            // Store the path of the textfile in your system 
+            string file = @"C:\Users\emese\source\repos\210223_Syntra_NETCORE_opdracht\210223_Syntra_NETCORE_opdracht\TextFile\MijnGetal.txt";
+
+            // To write all of the text to the file
+            System.IO.File.WriteAllText(file, aText);
+            return Ok(Convert.ToInt32(aText));
+        }
+
+
+
+        [HttpPost("Post2")]
+
+        public ActionResult<int> schrijfRandomGetal()
+        {
+            var random = new Random();
+            int randomnumber = random.Next(10);
+
+            // Store the path of the textfile in your system 
+            string file = @"C:\Users\emese\source\repos\210223_Syntra_NETCORE_opdracht\210223_Syntra_NETCORE_opdracht\TextFile\MijnGetal.txt";
+
+            // To write all of the text to the file
+            System.IO.File.WriteAllText(file, Convert.ToString(randomnumber));
+            return Ok(Convert.ToInt32(randomnumber));
+        }
+
+
+
+
+
+
     }
 }
