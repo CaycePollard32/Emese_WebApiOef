1bb4215ad21a0b6810965dbc5527af6bf405c839
CaycePollard32 <CaycePollard32@users.noreply.github.com>
2021-02-23 14:00:34 +0100

    Add project files.

---
+++ 210223_Syntra_NETCORE_opdracht/Controllers/GetalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _210223_Syntra_NETCORE_opdracht.Controllers
+{
+    public class GetalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
